7345e524ec298701f6396c23c49f4b6d4f598756
Andrey <40640793+ProgNext@users.noreply.github.com>
2022-03-17 11:12:07 +0500

    Edit assert

--- test_items.py
+++ test_items.py
@@ -6,5 +6,7 @@ def test_check_exists_button_add_in_card(browser, request):
     link = 'http://selenium1py.pythonanywhere.com/' + request.config.getoption("language") + '/catalogue/coders-at-work_207/'
     browser.get(link)
     time.sleep(10)
-    button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
-    assert button.get_attribute('type') == 'submit', "Error, button 'Add in card' is not exists!!!"
+    try:
+        button = browser.find_element(By.CLASS_NAME, "btn-add-to-baskett")
+    except:
+        assert False, "Error, button 'Add in card' is not exists!!!"
